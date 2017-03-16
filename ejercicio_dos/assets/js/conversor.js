fc93e235cd31cc953946274b8d7fe28bc1275d8d
Daffyta <daffy.dine9@gmail.com>
2017-03-16 10:14:26 -0600

    Leccion-14.2

--- ejercicio_dos/assets/js/conversor.js
+++ ejercicio_dos/assets/js/conversor.js
@@ -1,1 +1,12 @@
-document.getElementById('farenheit').value
+//document.getElementById('farenheit').value
+
+function convert(degree) {
+    var x;
+    if (degree == "C") {
+        x = document.getElementById("c").value * 9 / 5 + 32;
+        document.getElementById("f").value = Math.round(x);
+    } else {
+        x = (document.getElementById("f").value -32) * 5 / 9;
+        document.getElementById("c").value = Math.round(x);
+    }
+}
